80bc32c31672638d10113e42d6a6f8ad3d999d68
ideakkkk <97647960+ideakkkk@users.noreply.github.com>
2022-01-13 10:18:24 +0800

    Add files via upload

---
+++ weiyi/config.js
@@ -0,0 +1,33 @@
+
+var config = {
+  
+    texts: [
+         "送给我",      
+        "心爱的小可爱",  
+        "今天是你的生日",
+        "这是我们认识的",
+        "第1840天了哦",
+        "希望你在以后的日子里",
+        "天天快乐，没有烦恼",
+        "做一个无忧无虑的开心果",
+        "要把可爱维维喂饱饱",
+        "生日快乐哦",
+        "YAMI~~",
+    ],
+    
+    imgs: {
+        "心爱的小可爱": "./imgs/xiaokeai.png",
+        
+    },
+   
+    desc: {
+        turn_on: "开始",
+        play: "音乐",
+        bannar_coming: "彩带",
+        balloons_flying: "好像少点东西",
+        cake_fadein: "蛋糕？",
+        light_candle: "蜡烛？",
+        wish_message: "生日快乐",
+        story: "HAPPY BIRTHDAY TO YOU",
+    }
+};
